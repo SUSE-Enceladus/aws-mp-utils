666579fac291e075aeb6f13a4f987be97002079a
Sean Marlow <sean.marlow@suse.com>
2025-08-01 15:36:00 -0400

    Migrate code from existing tools
    Pulling code from brewmaster, mash, scripts, and growler.

--- aws_mp_utils/changeset.py
+++ aws_mp_utils/changeset.py
@@ -20,3 +20,411 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+import re
+import time
+
+import jmespath
+
+import boto3
+import botocore.exceptions as boto_exceptions
+
+from aws_mp_utils.exceptions import AWSMPUtilsException
+
+
+def get_change_set(client: boto3.client, change_set_id: str) -> dict:
+    """
+    Returns a dictionary containing changeset information
+
+    The changeset is found based on the id.
+    """
+    response = client.describe_change_set(
+        Catalog='AWSMarketplace',
+        ChangeSetId=change_set_id
+    )
+    return response
+
+
+def create_restrict_version_change_doc(
+    entity_id: str,
+    delivery_option_id: str
+) -> dict:
+    """
+    Creates a restrict delivery option request dictionary.
+
+    For use with submitting a changeset to delete an image
+    or container version from a product.
+    """
+    data = {
+        'ChangeType': 'RestrictDeliveryOptions',
+        'Entity': {
+            'Type': 'AmiProduct@1.0',
+            'Identifier': entity_id
+        }
+    }
+    details = {
+        'DeliveryOptionIds': [delivery_option_id]
+    }
+
+    data['Details'] = json.dumps(details)
+    return data
+
+
+def create_add_version_change_doc(
+    entity_id: str,
+    version_title: str,
+    ami_id: str,
+    access_role_arn: str,
+    release_notes: str,
+    os_name: str,
+    os_version: str,
+    usage_instructions: str,
+    recommended_instance_type: str,
+    ssh_user: str,
+) -> dict:
+    data = {
+        'ChangeType': 'AddDeliveryOptions',
+        'Entity': {
+            'Type': 'AmiProduct@1.0',
+            'Identifier': entity_id
+        }
+    }
+
+    details = {
+        'Version': {
+            'VersionTitle': version_title,
+            'ReleaseNotes': release_notes
+        },
+        'DeliveryOptions': [{
+            'Details': {
+                'AmiDeliveryOptionDetails': {
+                    'UsageInstructions': usage_instructions,
+                    'RecommendedInstanceType': recommended_instance_type,
+                    'AmiSource': {
+                        'AmiId': ami_id,
+                        'AccessRoleArn': access_role_arn,
+                        'UserName': ssh_user,
+                        'OperatingSystemName': os_name,
+                        'OperatingSystemVersion': os_version
+                    },
+                    'SecurityGroups': [{
+                        'FromPort': 22,
+                        'IpProtocol': 'tcp',
+                        'IpRanges': ['0.0.0.0/0'],
+                        'ToPort': 22
+                    }]
+                }
+            }
+        }]
+    }
+
+    data['Details'] = json.dumps(details)
+    return data
+
+
+def start_mp_change_set(
+    client: boto3.client,
+    change_set: str,
+    max_rechecks: int = 10,
+    rechecks_period: int = 900,
+    conflict_wait_period: int = 1800
+) -> dict:
+    """
+    Additional params included in this function:
+    - max_rechecks is the maximum number of checks that are
+    performed when a marketplace change cannot be applied because some resource
+    is affected by some other ongoing change (and ResourceInUseException is
+    raised by boto3).
+    - rechecks_period is the period (in seconds) that is waited
+    between checks for the ongoing mp change to be finished (defaults to 900s).
+    """
+    retries = 3
+    conflicting_changeset_retries = 10
+    while retries > 0:
+        conflicting_changeset = False
+        conflicting_error_message = ''
+        try:
+            response = client.start_change_set(
+                Catalog='AWSMarketplace',
+                ChangeSet=change_set
+            )
+            return response
+
+        except boto_exceptions.ClientError as error:
+            if error.response['Error']['Code'] == 'ResourceInUseException':
+                # Conflicting changeset for some resource
+                conflicting_changeset = True
+                conflicting_error_message = str(error)
+            else:
+                raise
+
+        if conflicting_changeset:
+            conflicting_changeset = False
+            time.sleep(conflict_wait_period)
+            conflicting_changeset_retries -= 1
+            if conflicting_changeset_retries <= 0:
+                try:
+                    ongoing_change_id = get_ongoing_change_id_from_error(
+                        conflicting_error_message
+                    )
+                    raise AWSMPUtilsException(
+                        'Unable to complete successfully the mp change.'
+                        f' Timed out waiting for {ongoing_change_id}'
+                        ' to finish.'
+                    )
+                except Exception:
+                    raise
+        else:
+            retries -= 1
+
+    raise AWSMPUtilsException(
+        'Unable to complete successfully the mp change.'
+    )
+
+
+def get_ongoing_change_id_from_error(message: str) -> str:
+    re_change_id = r'change sets: (\w{25})'
+    match = re.search(re_change_id, message)
+
+    if match:
+        change_id = match.group(1)
+        return change_id
+    else:
+        raise AWSMPUtilsException(
+            f'Unable to extract changeset id from aws err response: {message}'
+        )
+
+
+def get_changeset_status(
+    client: boto3.client,
+    changeset_id: str
+) -> str:
+    """Gets the status of the changeset"""
+    response = get_change_set(
+        client,
+        changeset_id
+    )
+    if response and 'Status' in response:
+        # 'Status':'PREPARING'|'APPLYING'|'SUCCEEDED'|'CANCELLED'|'FAILED'
+        status = response['Status'].lower()
+        return status
+
+
+def gen_add_delivery_options_changeset(
+    entity_id: str,
+    version_title: str,
+    release_notes: str,
+    delivery_option_title: str,
+    compatible_services: list,
+    container_images: list,
+    helm_chart_uri: str,
+    helm_chart_description: str,
+    usage_instructions: str,
+    quick_launch_enabled: bool,
+    marketplace_service_account_name: str,
+    release_name: str,
+    namespace: str,
+    override_parameters: list
+) -> dict:
+    """
+    Function to generate a marketplace changeset of AddDeliveryOptions type
+    https://docs.aws.amazon.com/marketplace-catalog/latest/api-reference/
+    container-products.html#working-with-container-products
+    """
+
+    data = {
+        'ChangeType': 'AddDeliveryOptions',
+        'Entity': {
+            'Type': 'ContainerProduct@1.0',
+            'Identifier': entity_id
+        }
+    }
+
+    details = {
+        'Version': {
+            'VersionTitle': version_title,
+            'ReleaseNotes': release_notes
+        },
+        'DeliveryOptions': [{
+            'DeliveryOptionTitle': delivery_option_title,
+            'Details': {
+                'HelmDeliveryOptionDetails': {
+                    'CompatibleServices': compatible_services,
+                    'ContainerImages': container_images,
+                    'HelmChartUri': helm_chart_uri,
+                    'Description': helm_chart_description,
+                    'UsageInstructions': usage_instructions,
+                    'QuickLaunchEnabled': quick_launch_enabled,
+                    'MarketplaceServiceAccountName':
+                        marketplace_service_account_name,
+                    'ReleaseName': release_name,
+                    'Namespace': namespace,
+                    'OverrideParameters': override_parameters
+                }
+            }
+        }]
+    }
+
+    data['Details'] = json.dumps(details)
+    return [data]
+
+
+def gen_update_delivery_options_changeset(
+    entity_id: str,
+    version_title: str,
+    release_notes: str,
+    delivery_option_title: str,
+    compatible_services: list,
+    container_images: list,
+    helm_chart_uri: str,
+    helm_chart_description: str,
+    usage_instructions: str,
+    quick_launch_enabled: bool,
+    marketplace_service_account_name: str,
+    release_name: str,
+    namespace: str,
+    override_parameters: list,
+    delivery_option_id: str
+) -> dict:
+    """
+    Function to generate a marketplace changeset of UpdateDeliveryOptions
+    type https://docs.aws.amazon.com/marketplace-catalog/latest/
+    api-reference/container-products.html#working-with-container-products
+    """
+
+    data = {
+        'ChangeType': 'UpdateDeliveryOptions',
+        'Entity': {
+            'Type': 'ContainerProduct@1.0',
+            'Identifier': entity_id
+        }
+    }
+
+    details = {
+        'Version': {
+            'VersionTitle': version_title,
+            'ReleaseNotes': release_notes
+        },
+        'DeliveryOptions': [{
+            'Id': delivery_option_id,
+            'Details': {
+                'HelmDeliveryOptionDetails': {
+                    'DeliveryOptionTitle': delivery_option_title,
+                    'CompatibleServices': compatible_services,
+                    'ContainerImages': container_images,
+                    'HelmChartUri': helm_chart_uri,
+                    'Description': helm_chart_description,
+                    'UsageInstructions': usage_instructions,
+                    'QuickLaunchEnabled': quick_launch_enabled,
+                    'MarketplaceServiceAccountName':
+                        marketplace_service_account_name,
+                    'ReleaseName': release_name,
+                    'Namespace': namespace,
+                    'OverrideParameters': override_parameters
+                }
+            }
+        }]
+    }
+
+    data['Details'] = json.dumps(details)
+    return [data]
+
+
+def get_helm_delivery_option_id(
+    client: boto3.client,
+    entity_id: str,
+    version_title: str
+) -> str:
+    """
+    Provides the id of the delivery option to be updated. The id of
+    the delivery option is selected from the version of the product
+    that matches the version_title provided. The delivery option is
+    the first delivery option of the type 'Helm' in that version.
+
+    Example describe entity output:
+
+    {
+        "Details": {
+            "Versions": [
+                {
+                    "VersionTitle": "Product 1.2.3",
+                    "DeliveryOptions": [
+                        {
+                            "Id": "4321",
+                            "Type": "Helm"
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+    """
+    entity = client.describe_entity(
+        Catalog='AWSMarketplace',
+        EntityId=entity_id
+    )
+    details = entity['DetailsDocument']
+
+    result = jmespath.search(
+        f"Versions[?VersionTitle=='{version_title}'] | [0]"
+        ".DeliveryOptions[?Type=='Helm'] | [0].Id",
+        details
+    )
+    return result
+
+
+def get_image_delivery_option_id(
+    client: boto3.client,
+    entity_id: str,
+    ami_id: str
+) -> str:
+    """
+    Return delivery option id for image matching ami id in given offer
+    """
+    entity = client.describe_entity(
+        Catalog='AWSMarketplace',
+        EntityId=entity_id
+    )
+
+    """
+    Example describe entity output:
+
+    {
+        "Details": {
+            "Versions": [
+                {
+                    "Sources": [
+                        {
+                            "Image": "ami-123",
+                            "Id": "1234"
+                        }
+                    ],
+                    "DeliveryOptions": [
+                        {
+                            "Id": "4321",
+                            "SourceId": "1234"
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+    """
+    details = entity['DetailsDocument']
+
+    source_id = jmespath.search(
+        f"Versions[?Sources[?Image=='{ami_id}']] | [0].Sources | [0].Id",
+        details
+    )
+
+    if not source_id:
+        return None
+
+    delivery_option_id = jmespath.search(
+        f"Versions[?DeliveryOptions[?SourceId=='{source_id}']] | "
+        "[0].DeliveryOptions | [0].Id",
+        details
+    )
+
+    return delivery_option_id
---
+++ tests/test_changeset.py
@@ -0,0 +1,452 @@
+import json
+
+import botocore.session
+import botocore.errorfactory
+import pytest
+
+from unittest.mock import Mock, call
+
+from aws_mp_utils.changeset import (
+    start_mp_change_set,
+    create_add_version_change_doc,
+    create_restrict_version_change_doc,
+    get_image_delivery_option_id,
+    get_helm_delivery_option_id,
+    get_change_set,
+    get_changeset_status,
+    gen_add_delivery_options_changeset,
+    gen_update_delivery_options_changeset
+)
+
+# botocore ClientExceptionFactory
+model = botocore.session.get_session().get_service_model('marketplace-catalog')
+factory = botocore.errorfactory.ClientExceptionsFactory()
+exceptions = factory.create_client_exceptions(model)
+
+# Mock calls parameters
+get_client_args = (
+    'marketplace-catalog',
+    '123456',
+    '654321',
+    'us-east-1'
+)
+data = {
+    'ChangeType': 'AddDeliveryOptions',
+    'Entity': {
+        'Type': 'AmiProduct@1.0',
+        'Identifier': '123'
+    }
+}
+details = {
+    'Version': {
+        'VersionTitle': 'New image',
+        'ReleaseNotes': 'Release Notes'
+    },
+    'DeliveryOptions': [{
+        'Details': {
+            'AmiDeliveryOptionDetails': {
+                'UsageInstructions': 'Login with SSH...',
+                'RecommendedInstanceType': 't3.medium',
+                'AmiSource': {
+                    'AmiId': 'ami-123',
+                    'AccessRoleArn': 'arn',
+                    'UserName': 'ec2-user',
+                    'OperatingSystemName': 'OTHERLINUX',
+                    'OperatingSystemVersion': '15.3'
+                },
+                'SecurityGroups': [{
+                    'FromPort': 22,
+                    'IpProtocol': 'tcp',
+                    'IpRanges': ['0.0.0.0/0'],
+                    'ToPort': 22
+                }]
+            }
+        }
+    }]
+}
+data['Details'] = json.dumps(details)
+start_changeset_params = {
+    'Catalog': 'AWSMarketplace',
+    'ChangeSet': [data]
+}
+describe_changeset_params = {
+    'Catalog': 'AWSMarketplace',
+    'ChangeSetId': 'dgoddlepi9nb3ynwrwlkr3be4'
+}
+
+
+def test_start_mp_change_set():
+    client = Mock()
+    client.start_change_set.return_value = {
+        'ChangeSetId': '123'
+    }
+
+    change_set = create_add_version_change_doc(
+        entity_id='123',
+        version_title='New image',
+        ami_id='ami-123',
+        access_role_arn='arn',
+        release_notes='Release Notes',
+        os_name='OTHERLINUX',
+        os_version='15.3',
+        usage_instructions='Login with SSH...',
+        recommended_instance_type='t3.medium',
+        ssh_user='ec2-user'
+    )
+
+    response = start_mp_change_set(
+        client,
+        [change_set]
+    )
+
+    assert response['ChangeSetId'] == '123'
+    client.start_change_set.assert_called_once_with(**start_changeset_params)
+
+
+def test_start_mp_change_set_ongoing_change_ResourceInUseException():
+
+    def generate_exception():
+        error_code = 'ResourceInUseException'
+        error_message = (
+            "Requested change set has entities locked by change sets"
+            " - entity: '6066beac-a43b-4ad0-b5fe-f503025e4747' "
+            " change sets: dgoddlepi9nb3ynwrwlkr3be4"
+        )
+        exc_data = {
+            "Error": {
+                "Code": error_code,
+                "Message": error_message
+            },
+            "ResponseMetadata": {
+                "RequestId": "aaaabbbb-cccc-dddd-eeee-ffff00001111",
+                "HTTPStatusCode": 400,
+                "HTTPHeaders": {
+                    "transfer-encoding": "chunked",
+                    "date": "Fri, 01 Jan 2100 00:00:00 GMT",
+                    "connection": "close",
+                    "server": "AmazonEC2"
+                },
+                "RetryAttempts": 0
+            }
+        }
+
+        return exceptions.AccessDeniedException(
+            error_response=exc_data,
+            operation_name='start_change_set'
+        )
+
+    client = Mock()
+    client.exceptions.AccessDeniedException = exceptions.AccessDeniedException
+    client.start_change_set.side_effect = generate_exception()
+
+    change_set = create_add_version_change_doc(
+        entity_id='123',
+        version_title='New image',
+        ami_id='ami-123',
+        access_role_arn='arn',
+        release_notes='Release Notes',
+        os_name='OTHERLINUX',
+        os_version='15.3',
+        usage_instructions='Login with SSH...',
+        recommended_instance_type='t3.medium',
+        ssh_user='ec2-user'
+    )
+
+    with pytest.raises(Exception) as error:
+        start_mp_change_set(
+            client,
+            change_set=[change_set],
+            max_rechecks=10,
+            rechecks_period=0,
+            conflict_wait_period=0
+        )
+    assert 'AWSMPUtilsException' in str(error)
+    client.start_change_set.assert_has_calls(
+        [
+            call(**start_changeset_params),
+            call(**start_changeset_params)
+        ],
+        any_order=True
+    )
+
+
+def test_start_mp_change_set_ongoing_change_GenericBotoException():
+
+    def generate_exception():
+        error_code = 'AccessDeniedException'
+        error_message = "AccessDeniedException"
+        exc_data = {
+            "Error": {
+                "Code": error_code,
+                "Message": error_message
+            },
+            "ResponseMetadata": {
+                "RequestId": "aaaabbbb-cccc-dddd-eeee-ffff00001111",
+                "HTTPStatusCode": 400,
+                "HTTPHeaders": {
+                    "transfer-encoding": "chunked",
+                    "date": "Fri, 01 Jan 2100 00:00:00 GMT",
+                    "connection": "close",
+                    "server": "AmazonEC2"
+                },
+                "RetryAttempts": 0
+            }
+        }
+
+        return exceptions.AccessDeniedException(
+            error_response=exc_data,
+            operation_name='start_change_set'
+        )
+
+    client = Mock()
+    client.exceptions.AccessDeniedException = exceptions.AccessDeniedException
+    client.start_change_set.side_effect = generate_exception()
+
+    change_set = create_add_version_change_doc(
+        entity_id='123',
+        version_title='New image',
+        ami_id='ami-123',
+        access_role_arn='arn',
+        release_notes='Release Notes',
+        os_name='OTHERLINUX',
+        os_version='15.3',
+        usage_instructions='Login with SSH...',
+        recommended_instance_type='t3.medium',
+        ssh_user='ec2-user'
+    )
+
+    with pytest.raises(Exception) as error:
+        start_mp_change_set(
+            client,
+            change_set=[change_set],
+            max_rechecks=10,
+            rechecks_period=0,
+            conflict_wait_period=0
+        )
+    assert 'AccessDeniedException' in str(error)
+    client.start_change_set.assert_has_calls(
+        [
+            call(**start_changeset_params),
+        ],
+    )
+
+
+def test_create_restrict_version_change_doc():
+    expected = {
+        'ChangeType': 'RestrictDeliveryOptions',
+        'Entity': {
+            'Type': 'AmiProduct@1.0',
+            'Identifier': '123456789'
+        }
+    }
+    details = {
+        'DeliveryOptionIds': ['987654321']
+    }
+    expected['Details'] = json.dumps(details)
+
+    actual = create_restrict_version_change_doc('123456789', '987654321')
+    assert expected == actual
+
+
+def test_get_image_delivery_option_id():
+    details = {
+        "Versions": [
+            {
+                "Sources": [
+                    {
+                        "Image": "ami-123",
+                        "Id": "1234"
+                    }
+                ],
+                "DeliveryOptions": [
+                    {
+                        "Id": "4321",
+                        "SourceId": "1234"
+                    }
+                ]
+            }
+        ]
+    }
+
+    entity = {
+        'DetailsDocument': details
+    }
+    client = Mock()
+    client.describe_entity.return_value = entity
+
+    did = get_image_delivery_option_id(
+        client,
+        '1234589',
+        'ami-123',
+    )
+    assert did == '4321'
+
+    # Test no image match found
+    details['Versions'][0]['Sources'][0]['Image'] = 'ami-321'
+    entity['Details'] = json.dumps(details)
+
+    did = get_image_delivery_option_id(
+        client,
+        '1234589',
+        'ami-123',
+    )
+    assert did is None
+
+
+def test_get_helm_delivery_option_id():
+    details = {
+        "Versions": [
+            {
+                "VersionTitle": "Product 1.2.3",
+                "DeliveryOptions": [
+                    {
+                        "Id": "4321",
+                        "Type": "Helm"
+                    }
+                ]
+            }
+        ]
+    }
+
+    entity = {
+        'DetailsDocument': details
+    }
+    client = Mock()
+    client.describe_entity.return_value = entity
+
+    did = get_helm_delivery_option_id(
+        client,
+        '1234589',
+        'Product 1.2.3',
+    )
+    assert did == '4321'
+
+    # Test no image match found
+    details['Versions'][0]['VersionTitle'] = 'Product 1.2.4'
+    entity['Details'] = json.dumps(details)
+
+    did = get_helm_delivery_option_id(
+        client,
+        '1234589',
+        'Product 1.2.3',
+    )
+    assert did is None
+
+
+def test_get_change_set():
+    response = {
+        'ChangeSetId': '123',
+        'ChangeSetArn': 'string',
+        'ChangeSetName': 'string',
+        'Intent': 'APPLY',
+        'StartTime': '2018-02-27T13:45:22Z',
+        'EndTime': '2018-02-27T13:45:22Z',
+        'Status': 'SUCCEEDED',
+        'ChangeSet': [
+            {
+                'ChangeType': 'string',
+                'Entity': {
+                    'Type': 'string',
+                    'Identifier': 'string'
+                },
+                'Details': 'string',
+                'DetailsDocument': {'changeset': 'details'},
+                'ChangeName': 'string'
+            },
+        ]
+    }
+
+    client = Mock()
+    client.describe_change_set.return_value = response
+
+    response = get_change_set(client, '123')
+    assert response['ChangeSetId'] == '123'
+
+
+def test_get_changeset_status():
+    response = {
+        'Status': 'SUCCEEDED',
+        'ChangeSet': [
+            {
+                'ChangeType': 'string',
+                'Entity': {
+                    'Type': 'string',
+                    'Identifier': 'string'
+                },
+                'Details': 'string',
+                'DetailsDocument': {'changeset': 'details'},
+                'ChangeName': 'string'
+            },
+        ]
+    }
+
+    client = Mock()
+    client.describe_change_set.return_value = response
+
+    response = get_changeset_status(client, '123')
+    assert response == 'succeeded'
+
+
+def test_gen_add_delivery_options_changeset():
+    response = gen_add_delivery_options_changeset(
+        '123',
+        'Chart 1.2.3',
+        'Release Notes...',
+        'Chart - 1.2.3',
+        ['EKS'],
+        ['123.dkr.ecr.us-east-1.amazonaws.com/sellername/reponame1:1'],
+        '123.dkr.ecr.us-east-1.amazonaws.com/sellername/reponame1:helmchart1',
+        'Helm chart description',
+        'Usage instructions',
+        True,
+        'Service account name',
+        'Optional release name',
+        'Optional k8s namespace',
+        [
+            {
+                "Key": "HelmKeyName1",
+                "DefaultValue": "${AWSMP_LICENSE_SECRET}",
+                "Metadata":
+                {
+                    "Label": "AWS CloudFormation template field label",
+                    "Description": "AWS CloudFormation field description",
+                    "Obfuscate": False
+                }
+            }
+        ]
+    )
+    assert 'Details' in response[0]
+    assert response[0]['ChangeType'] == 'AddDeliveryOptions'
+
+
+def test_gen_update_delivery_options_changeset():
+    response = gen_update_delivery_options_changeset(
+        '123',
+        'Chart 1.2.3',
+        'Release Notes...',
+        'Chart - 1.2.3',
+        ['EKS'],
+        ['123.dkr.ecr.us-east-1.amazonaws.com/sellername/reponame1:1'],
+        '123.dkr.ecr.us-east-1.amazonaws.com/sellername/reponame1:helmchart1',
+        'Helm chart description',
+        'Usage instructions',
+        True,
+        'Service account name',
+        'Optional release name',
+        'Optional k8s namespace',
+        [
+            {
+                "Key": "HelmKeyName1",
+                "DefaultValue": "${AWSMP_LICENSE_SECRET}",
+                "Metadata":
+                {
+                    "Label": "AWS CloudFormation template field label",
+                    "Description": "AWS CloudFormation field description",
+                    "Obfuscate": False
+                }
+            }
+        ],
+        '123-321'
+    )
+    assert 'Details' in response[0]
+    assert response[0]['ChangeType'] == 'UpdateDeliveryOptions'
