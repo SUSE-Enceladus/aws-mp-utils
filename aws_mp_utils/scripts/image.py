6685565b2c8621465ed8aa01e3ef70b03e0bdfa6
Sean Marlow <sean.marlow@suse.com>
2025-08-06 17:16:36 -0400

    Add functions and cli command
    To restrict image version from a product.
    
    Also, add functions to start marketplace change set and create
    add delivery option change doc.

--- aws_mp_utils/scripts/image.py
+++ aws_mp_utils/scripts/image.py
@@ -21,13 +21,132 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
+import sys
+
 import click
+
+from aws_mp_utils.changeset import (
+    get_image_delivery_option_id,
+    create_restrict_version_change_doc,
+    start_mp_change_set
+)
+from aws_mp_utils.scripts.cli_utils import (
+    add_options,
+    get_config,
+    process_shared_options,
+    shared_options,
+    echo_style,
+    get_mp_client
+)
 
 
 # -----------------------------------------------------------------------------
-# Offer commands function
+# Image commands function
 @click.group(name="image")
 def image():
     """
     Commands for marketplace catalog AMI product management.
     """
+
+
+# -----------------------------------------------------------------------------
+@image.command
+@click.option(
+    '--intent',
+    type=click.Choice(['VALIDATE', 'APPLY']),
+    help='To test your request before applying changes to your entities, use '
+         'VALIDATE. The default is APPLY. This feature is currently available'
+         ' for adding versions to single-AMI products.'
+)
+@click.option(
+    '--max-rechecks',
+    type=click.IntRange(min=0),
+    help='The maximum number of checks that are performed when a marketplace '
+         'change cannot be applied because some resource is affected by some '
+         'other ongoing change.'
+)
+@click.option(
+    '--conflict-wait-period',
+    type=click.IntRange(min=0),
+    help='The period (in seconds) that is waited between checks for the '
+         'ongoing mp change to be finished.'
+)
+@click.option(
+    '--entity-id',
+    type=click.STRING,
+    required=True,
+    help='The unique identifier the product in the AWS Marketplace. '
+         'The expected format of the ID is a UUID.'
+)
+@click.option(
+    '--ami-id',
+    type=click.STRING,
+    required=True,
+    help='The EC2 image ID for the version to be restricted.'
+)
+@add_options(shared_options)
+@click.pass_context
+def restrict_version(
+    context,
+    ami_id,
+    entity_id,
+    conflict_wait_period,
+    max_rechecks,
+    intent,
+    **kwargs
+):
+    process_shared_options(context.obj, kwargs)
+    config_data = get_config(context.obj)
+    logger = logging.getLogger('aws_mp_utils')
+    logger.setLevel(config_data.log_level)
+
+    client = get_mp_client(
+        config_data.profile,
+        config_data.region
+    )
+    try:
+        delivery_option_id = get_image_delivery_option_id(
+            client,
+            entity_id,
+            ami_id
+        )
+    except Exception as error:
+        echo_style(
+            'Unable to get delivery option id for the given product.',
+            config_data.no_color,
+            fg='red'
+        )
+        echo_style(str(error), config_data.no_color, fg='red')
+        sys.exit(1)
+
+    change_doc = create_restrict_version_change_doc(
+        entity_id,
+        delivery_option_id
+    )
+
+    options = {
+        'client': client,
+        'change_set': [change_doc]
+    }
+
+    if max_rechecks:
+        options['max_rechecks'] = max_rechecks
+    if conflict_wait_period:
+        options['conflict_wait_period'] = conflict_wait_period
+    if intent:
+        options['intent'] = intent
+
+    try:
+        response = start_mp_change_set(**options)
+    except Exception as error:
+        echo_style(
+            'Unable to start change set',
+            config_data.no_color,
+            fg='red'
+        )
+        echo_style(str(error), config_data.no_color, fg='red')
+        sys.exit(1)
+
+    output = f'Change set Id: {response["ChangeSetId"]}'
+    echo_style(output, config_data.no_color, fg='green')
---
+++ tests/test_cli_image.py
@@ -0,0 +1,49 @@
+from unittest.mock import patch
+
+from click.testing import CliRunner
+
+from aws_mp_utils.scripts.cli import main
+
+
+# -------------------------------------------------
+@patch('aws_mp_utils.scripts.image.start_mp_change_set')
+@patch('aws_mp_utils.scripts.image.get_image_delivery_option_id')
+@patch('aws_mp_utils.scripts.image.get_mp_client')
+def test_restrict_version(
+    mock_client,
+    mock_get_delivery_id,
+    mock_start_change_set
+):
+    """Confirm restrict image version"""
+    mock_get_delivery_id.return_value = '12345'
+    mock_start_change_set.return_value = {
+        'ChangeSetId': '123456789'
+    }
+
+    args = [
+        'image', 'restrict-version',
+        '--config-file', 'tests/data/config.yaml',
+        '--entity-id', '00000000-0000-4000-8000-000000000001',
+        '--ami-id', 'ami-12345',
+        '--intent', 'VALIDATE',
+        '--max-rechecks', 10,
+        '--conflict-wait-period', 300,
+        '--no-color'
+    ]
+
+    runner = CliRunner()
+    result = runner.invoke(main, args)
+    assert result.exit_code == 0
+    assert 'Change set Id: 123456789' in result.output
+
+    # Failure to start changeset
+    mock_start_change_set.side_effect = Exception('Invalid change set!')
+    result = runner.invoke(main, args)
+    assert result.exit_code == 1
+    assert 'Invalid change set!' in result.output
+
+    # Simulate failure in boto3
+    mock_get_delivery_id.side_effect = Exception('403: Auth failure!')
+    result = runner.invoke(main, args)
+    assert result.exit_code == 1
+    assert '403: Auth failure!' in result.output
